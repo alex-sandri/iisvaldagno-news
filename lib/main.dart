b7afb559ac100c7f26345f0d351ec7d4fdfb9488
Alex Sandri <me@alexsandri.com>
2020-08-15 18:51:16 +0200

    Hide debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Container(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
