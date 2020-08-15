11a289bfce4cfd1eef367c7b241a198ea96123b4
Alex Sandri <me@alexsandri.com>
2020-08-15 20:00:48 +0200

    Make link text selectable

--- lib/main.dart
+++ lib/main.dart
@@ -105,7 +105,7 @@ class _HomeState extends State<Home> {
 
                             return ListTile(
                               dense: true,
-                              title: Text(
+                              title: SelectableText(
                                 link.text,
                               ),
                             );
