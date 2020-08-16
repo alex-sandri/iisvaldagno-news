01bb810cc9f7dbebcb90e289e3b4c795c4f0b552
Alex Sandri <me@alexsandri.com>
2020-08-16 11:51:32 +0200

    Add News route

---
+++ lib/news.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:dart_rss/dart_rss.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class News extends StatefulWidget {
+  final RssItem item;
+
+  News(this.item);
+
+  @override
+  _NewsState createState() => _NewsState();
+}
+
+class _NewsState extends State<News> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "TODO",
+            ),
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  text: "News",
+                ),
+                Tab(
+                  text: "Allegati",
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Container(),
+              Container(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
