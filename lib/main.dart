a0579cbec49532130edf09faf3b8b5a50c1248f5
Alex Sandri <me@alexsandri.com>
2020-09-08 19:45:57 +0200

    Fix: context does not have a Navigator

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final QuickActions quickActions = QuickActions();
-
-    quickActions.initialize((shortcutType) {
-      print(shortcutType);
-    });
-
-    quickActions.setShortcutItems([
-      const ShortcutItem(type: "action_evidenza", localizedTitle: "Notizie in evidenza"),
-      const ShortcutItem(type: "action_evidenza_iti", localizedTitle: "In evidenza ITI"),
-      const ShortcutItem(type: "action_evidenza_ite", localizedTitle: "In evidenza ITE"),
-      const ShortcutItem(type: "action_evidenza_ip", localizedTitle: "In evidenza IP"),
-    ]);
-
     return MaterialApp(
       title: "IIS Valdagno News",
       theme: ThemeData.light(),
@@ -53,7 +40,40 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   @override
+  void initState() {
+    super.initState();
+
+    final QuickActions quickActions = QuickActions();
+
+    quickActions.initialize((shortcutType) {
+      String url;
+
+      switch (shortcutType)
+      {
+        case "action_evidenza": url = "https://www.iisvaldagno.it/categorie/news"; break;
+        case "action_evidenza_iti": url = "https://www.iisvaldagno.it/categorie/news/iti-news"; break;
+        case "action_evidenza_ite": url = "https://www.iisvaldagno.it/categorie/news/ite-news"; break;
+        case "action_evidenza_ip": url = "https://www.iisvaldagno.it/categorie/news/ip-news"; break;
+      }
+
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => Home(url),
+        ),
+      );
+    });
+
+    quickActions.setShortcutItems([
+      const ShortcutItem(type: "action_evidenza", localizedTitle: "Notizie in evidenza"),
+      const ShortcutItem(type: "action_evidenza_iti", localizedTitle: "In evidenza ITI"),
+      const ShortcutItem(type: "action_evidenza_ite", localizedTitle: "In evidenza ITE"),
+      const ShortcutItem(type: "action_evidenza_ip", localizedTitle: "In evidenza IP"),
+    ]);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(widget.url);
     return Material(
       child: Scaffold(
         appBar: AppBar(
