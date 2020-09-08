9994279bebd9fc1fe36358d0893396ec51c8eb91
Alex Sandri <me@alexsandri.com>
2020-09-08 19:35:04 +0200

    Move quick actions initialization inside MyApp build

--- lib/main.dart
+++ lib/main.dart
@@ -5,27 +5,25 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:quick_actions/quick_actions.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  final QuickActions quickActions = QuickActions();
-
-  quickActions.initialize((shortcutType) {
-    print(shortcutType);
-  });
-
-  quickActions.setShortcutItems([
-    const ShortcutItem(type: "action_evidenza", localizedTitle: "Notizie in evidenza"),
-    const ShortcutItem(type: "action_evidenza_iti", localizedTitle: "In evidenza ITI"),
-    const ShortcutItem(type: "action_evidenza_ite", localizedTitle: "In evidenza ITE"),
-    const ShortcutItem(type: "action_evidenza_ip", localizedTitle: "In evidenza IP"),
-  ]);
-
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final QuickActions quickActions = QuickActions();
+
+    quickActions.initialize((shortcutType) {
+      print(shortcutType);
+    });
+
+    quickActions.setShortcutItems([
+      const ShortcutItem(type: "action_evidenza", localizedTitle: "Notizie in evidenza"),
+      const ShortcutItem(type: "action_evidenza_iti", localizedTitle: "In evidenza ITI"),
+      const ShortcutItem(type: "action_evidenza_ite", localizedTitle: "In evidenza ITE"),
+      const ShortcutItem(type: "action_evidenza_ip", localizedTitle: "In evidenza IP"),
+    ]);
+
     return MaterialApp(
       title: "IIS Valdagno News",
       theme: ThemeData.light(),
