b9f5ebaddd66bd07243c3eb2963917430d71bb2d
Alex Sandri <me@alexsandri.com>
2020-09-09 15:39:13 +0200

    Add initial categories

--- lib/news.dart
+++ lib/news.dart
@@ -24,7 +24,10 @@ class News extends StatefulWidget {
   News(this.item);
 
   static const Map<String, String> _categories = {
-    // TODO
+    "Notizie in evidenza": "https://www.iisvaldagno.it/categorie/news",
+    "In evidenza ITI": "https://www.iisvaldagno.it/categorie/news/iti-news",
+    "In evidenza ITE": "https://www.iisvaldagno.it/categorie/news/ite-news",
+    "In evidenza IP": "https://www.iisvaldagno.it/categorie/news/ip-news",
   };
 
   @override
@@ -162,16 +165,7 @@ class _NewsState extends State<News> {
                             )
                           : null,
                         onPressed: () {
-                          String url;
-
-                          switch (category.value)
-                          {
-                            case "Notizie in evidenza": url = "https://www.iisvaldagno.it/categorie/news"; break;
-                            case "In evidenza ITI": url = "https://www.iisvaldagno.it/categorie/news/iti-news"; break;
-                            case "In evidenza ITE": url = "https://www.iisvaldagno.it/categorie/news/ite-news"; break;
-                            case "In evidenza IP": url = "https://www.iisvaldagno.it/categorie/news/ip-news"; break;
-                            default: url = News._categories[category.value] ?? "https://www.iisvaldagno.it/tag/${category.value}"; break;
-                          }
+                          String url = News._categories[category.value] ?? "https://www.iisvaldagno.it/tag/${category.value}";
 
                           url += "/page/{{PAGE}}/?feed=rss2";
 
