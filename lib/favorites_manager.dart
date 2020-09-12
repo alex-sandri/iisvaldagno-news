3cde51af0a2521399a8bcb379f1ce31736dad095
Alex Sandri <me@alexsandri.com>
2020-09-12 12:59:58 +0200

    getAll doesn't return null

--- lib/favorites_manager.dart
+++ lib/favorites_manager.dart
@@ -7,7 +7,7 @@ class FavoritesManager
 
   static List<SerializableNews> getAll() => (_box.get("list") as List<dynamic>)?.whereType<SerializableNews>()?.toList() ?? [];
 
-  static Future<void> add(SerializableNews value) => _box.put("list", [ ...(getAll() ?? []), value ]);
+  static Future<void> add(SerializableNews value) => _box.put("list", [ ...getAll(), value ]);
 
   static Future<void> delete(SerializableNews value) async {
     final List<SerializableNews> favorites = getAll();
