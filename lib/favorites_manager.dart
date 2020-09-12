97deb0d0dddb0a54bf51c7673569ee02a71c6461
Alex Sandri <me@alexsandri.com>
2020-09-12 11:20:28 +0200

    Update pathd

--- lib/favorites_manager.dart
+++ lib/favorites_manager.dart
@@ -1,5 +1,5 @@
 import 'package:hive/hive.dart';
-import 'package:iisvaldagno_news/SerializableNews.dart';
+import 'package:iisvaldagno_news/models/SerializableNews.dart';
 
 class FavoritesManager
 {
